ebc5ee6dc9384a7694daa13687502fa468bcbdf0
El Panas <luca.panariello@gmail.com>
2021-04-29 19:51:31 +0200

    minor corrections

--- models/schemas.js
+++ models/schemas.js
@@ -5,7 +5,7 @@ const Schema = mongoose.Schema;
 const commentSchema = Schema({
     name: { type: String, required: true },
     mail: { type: String, default: '' },
-    content: { type: String, default: '' }
+    content: { type: String, required: true}
 });
 
 const Comment = mongoose.model('comment', commentSchema);

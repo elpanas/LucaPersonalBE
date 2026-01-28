3e6139125329cd00c2e2efedbc39c2e5d7ec5eec
Luca Panariello <luca.panariello@gmail.com>
2026-01-28 12:29:47 +0100

    ADD: vercel.json

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 /* eslint-disable no-console */
 require('./db/db'); // DATABASE CONNECTIONS
 const express = require('express');
-const path = require('path');
 // FRAMEWORK
 const app = express();
 const compression = require('compression'); // MIDDLEWARES
@@ -15,17 +14,6 @@ app.use(helmet());
 app.use(compression());
 app.use(cors(config.corsOpt));
 app.use(express.json());
-
-// Serve la doc Python
-app.use('/docs', express.static(path.join(__dirname, 'public/docs')));
-
-app.get('/docs', (req, res) => {
-  res.redirect('/docs/');
-});
-
-app.get('/docs/*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public/docs/index.html'));
-});
 
 // ROUTES
 app.use('/api/comment', restcomment);
